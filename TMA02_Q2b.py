a88147d6d803332d126c4a14237f64363a8c6576
Ross Hunter <rossxhunter@gmail.com>
2019-04-03 18:48:28 +0100

    Solution to Q2b

--- TMA02_Q2b.py
+++ TMA02_Q2b.py
@@ -11,16 +11,12 @@ class Bag:
 
     def __init__(self):
         """Create a new empty bag."""
-        bag = dict()
+        self.bag = dict()
         
 
     def add(self, item):
         """Add one copy of item to the bag. Multiple copies are allowed."""
-        if item not in words:
-            bag[item] = 1
-        else:
-            bag =+ 1
-        return bag
+        self.bag[len(self.bag)] = item
 
 
     def count(self, item):
@@ -28,19 +24,32 @@ class Bag:
         
         Return zero if the item doesn't occur in the bag.
         """
-        
+
+        counter = 0
+        for index, current_item in self.bag.items():
+            if current_item == item:
+                counter += 1
+        return counter
     
     def clear(self, item):
         """Remove all copies of item from the bag. 
         
         Do nothing if the item doesn't occur in the bag.
         """
+        for index, current_item in self.bag.items():
+            if current_item == item:
+                self.bag.pop(index, None)
                 
     def size(self):
         """Return the total number of copies of all items in the bag."""
+        return len(self.bag)
     
     def ordered(self):
         """Return the items by decreasing number of copies.
         
         Return a list of (count, item) pairs.
         """
+        result = set()
+        for index, item in self.bag.items():
+            result.add((self.count(item), item))
+        return sorted(result, reverse=True)
